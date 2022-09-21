09c7acae0946bb378eee757d7625315f6bfc875c
a-memme <andrew.memme@gmail.com>
2022-09-21 14:43:54 +0000

    test 2

--- dynamic_scheduling_app.py
+++ dynamic_scheduling_app.py
@@ -1242,7 +1242,7 @@ if choice == 'Episode Summary':
       gb = GridOptionsBuilder.from_dataframe(ag_df)
       gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
       gb.configure_side_bar() #Add a sidebar
-      gb.configure_column('Forecast % Against Avg', cellStyle=jscells)
+      #gb.configure_column('Forecast % Against Avg', cellStyle=jscells)
       gb.configure_column('Forecast % Against Average', suppressColumnsToolPanel=true)
 
       gridOptions = gb.build()
